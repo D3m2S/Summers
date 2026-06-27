fcb273bb7469be128194fe27ff94359539db3274
D3m2S <r2w2wzgz62@privaterelay.appleid.com>
2026-06-27 15:45:07 +0100

    Update theme.dart

--- lib/app/theme.dart
+++ lib/app/theme.dart
@@ -1,1 +1,30 @@
+import 'package:flutter/material.dart';
 
+class AppTheme {
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    brightness: Brightness.dark,
+
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: const Color(0xFF00C2FF),
+      brightness: Brightness.dark,
+    ),
+
+    scaffoldBackgroundColor: const Color(0xFF0B0F17),
+
+    appBarTheme: const AppBarTheme(
+      centerTitle: true,
+      backgroundColor: Color(0xFF161D2A),
+      foregroundColor: Colors.white,
+      elevation: 0,
+    ),
+
+    cardTheme: CardThemeData(
+      color: const Color(0xFF161D2A),
+      elevation: 4,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+    ),
+  );
+}
